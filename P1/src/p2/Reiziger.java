1dbce955b91154000b47c9f2ae283e3a68e54cdc
Tim <actualcookie@gmail.com>
2019-05-09 11:28:56 +0200

    Uitbreiding
    Update van de Reizger en DAO class

---
+++ P1/src/p2/Reiziger.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2;
+
+import p1.*;
+import java.sql.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author Tim
+ */
+public class Reiziger {
+    
+    /* 
+    om een ID aan te maken gebruik ik een static zodat deze automatisch 
+    optelt en stabiel blijft. Dit zal later moeten veranderen, omdat 
+    de counter reset na het sluiten van het programma. 
+    Voor nu zou het voldoende moeten zijn aangezien de rest 
+    van de data ook verloren gaat.*/
+    private static int counter = 0;
+    private String naam;
+    private Date gbdatum;
+    private int identifier;
+    
+    public Reiziger(){
+        /*this.naam = "";
+        this.gbdatum = java.sql.Date.valueOf("0000-01-01");*/
+        this.identifier = counter;
+        counter++;
+    }
+    
+    public String getNaam(){
+        return this.naam;
+    }
+    
+    public void setNaam(String nm){
+        this.naam = nm;
+    }
+    
+    
+    public Date getGBDatum(){
+        return this.gbdatum;
+    }
+    
+    /*   
+    Throws IO exception when a wrong string is inputted. 
+    >>Find fix<<
+    */
+    public void setGBDatum( String gb){
+        Date new_gb = java.sql.Date.valueOf(gb);
+        this.gbdatum = new_gb;
+    }
+    
+    public void setGBDatum( Date gb){
+        this.gbdatum = gb;
+    }
+    
+        public int getID(){
+        return this.identifier;
+    }
+        /*
+        Deze functie lijkt onnodig maar is dat niet.
+        Deze functie gaat in gebruik zodra de static counter 
+        niet meer voldoende is.
+        */
+    public void setID(int id){
+        this.identifier = id;
+    }
+    
+    @Override
+    public String toString(){
+        return this.naam + " : " + this.gbdatum + " > " + this.identifier;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        boolean equal = false;
+        if(obj == null){
+            return equal;
+        }
+        
+        if(obj instanceof Reiziger){
+            Reiziger temprg = (Reiziger)obj;
+            if(this.naam == null && temprg.getNaam() == null){
+                if(     this.gbdatum.equals(temprg.getGBDatum()) &&
+                        this.identifier == temprg.getID()){
+                            
+                    equal = true; 
+                }
+            }
+            else if(this.gbdatum == null && temprg.getGBDatum() == null){
+                if(this.naam.equals(temprg.getNaam()) && 
+                   this.identifier == temprg.getID()){
+                    equal = true;
+                }
+            }
+            else if (this.gbdatum != null &&
+                     temprg.getGBDatum() != null &&
+                     this.naam != null && 
+                     temprg.getNaam() != null){
+                
+                        if(this.naam.equals(temprg.getNaam()) && 
+                           this.gbdatum.equals(temprg.getGBDatum()) &&
+                           this.identifier == temprg.getID()){
+
+                            equal = true;
+                }
+            }
+        }
+        return equal;
+    }
+
+}
---
+++ P1/src/p2/ReizigerOracleDaoImpl.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2;
+
+import p1.*;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tim
+ * Aangezien 
+ */
+
+public class ReizigerOracleDaoImpl /*extends OracleDao*/ implements ReizigerDAO  {
+
+    ArrayList<Reiziger> alleReizigers = new ArrayList();
+    
+    @Override
+    public List<Reiziger> findall() {
+        return alleReizigers;
+    }
+
+    @Override
+    /*Aanname:
+        findByDate moet een lijst 
+        van alle reizigers opleveren met geboortedatums gelijk aan gbdatum.
+    */
+    public List<Reiziger> findByDate(String gbdatum) {
+        Date searchable = java.sql.Date.valueOf(gbdatum);
+        ArrayList <Reiziger> gbdatummatch = new ArrayList();
+        for(Reiziger rg : alleReizigers){
+           if(searchable != null && rg.getGBDatum() != null){
+                if(rg.getGBDatum().equals(searchable)){
+                    gbdatummatch.add(rg);
+                } 
+           }
+        }
+        return gbdatummatch;
+    }
+
+    @Override
+    public Reiziger save(Reiziger reiziger) {
+        
+        this.update(reiziger);
+        Reiziger r = this.copyReiziger(reiziger);
+        
+        
+        if(!alleReizigers.contains(r)){
+            alleReizigers.add(r);
+        } 
+        
+        return r;
+    }
+
+    @Override
+    /* Aanname:
+        update zoekt naar een reiziger met een bepaalde identifier 
+        en "update" deze met nieuwe informatie.*/
+    public Reiziger update(Reiziger reiziger) {
+        Reiziger r = new Reiziger();
+        if(reiziger == null)
+        {
+            return r;
+        }
+        for(Reiziger rg : alleReizigers){
+            if(reiziger.getID() == rg.getID()){
+                rg.setGBDatum(reiziger.getGBDatum());
+                rg.setNaam(reiziger.getNaam());
+                r = this.copyReiziger(reiziger);
+                break;
+            }
+        }
+        return r;
+    }
+
+    @Override
+    public boolean delete(Reiziger reiziger) {
+        boolean deleted = false;
+        if(alleReizigers.contains(reiziger)){
+            alleReizigers.remove(reiziger);
+            deleted = true;
+        }        
+        return deleted;
+    }
+
+    @Override
+    public void closeConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private Reiziger copyReiziger(Reiziger reiziger){
+
+        Reiziger r = new Reiziger();
+        if(reiziger == null){
+            return r;
+        }
+        r.setID(reiziger.getID());
+        r.setGBDatum(reiziger.getGBDatum());
+        r.setNaam(reiziger.getNaam());
+        return r;
+    }
+    
+    
+    
+}
